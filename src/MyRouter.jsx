1eeb9e4cfd57d02148275151348c845a7fd29cc2
Charlyln <charleshenry.lenue>
2020-07-22 13:47:29 +0200

    create home component and his route to myrouter

--- src/MyRouter.jsx
+++ src/MyRouter.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import SignUp from "./components/signUp/Signup";
+import Home from "./components/signUp/App/Home";
 
 const MyRouter = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={SignUp} />
+        <Route path="/home" component={Home} />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/components/signUp/App/Home.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+function Home() {
+  return (
+    <>
+      <h1>coucou</h1>
+    </>
+  );
+}
+
+export default Home;
