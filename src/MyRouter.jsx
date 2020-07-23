2b78979a697de8201e8856e2a2fd783d9d45fd38
Charlyln <charleshenry.lenue>
2020-07-23 03:17:04 +0200

    add new routes to my router

--- src/MyRouter.jsx
+++ src/MyRouter.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import SignUp from "./components/signUp/Signup";
-import Home from "./components/signUp/App/Home";
+import Home from "./components/App/Home";
+import MyAppBar from "./components/signUp/appBar/MyAppBar";
+import Profil from "./components/App/Profil";
+import Post from "./components/App/Post";
+import Favorite from "./components/App/Favorite";
+import Booking from "./components/App/Booking";
 
 const MyRouter = () => {
   return (
     <BrowserRouter>
+      <MyAppBar />
       <Switch>
         <Route exact path="/" component={SignUp} />
         <Route path="/home" component={Home} />
+        <Route path="/profil" component={Profil} />
+        <Route path="/post" component={Post} />
+        <Route path="/favorite" component={Favorite} />
+        <Route path="/bookings" component={Booking} />
       </Switch>
     </BrowserRouter>
   );
