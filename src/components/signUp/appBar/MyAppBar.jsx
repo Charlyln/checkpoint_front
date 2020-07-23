d25fc3e1d35389d85d3292f1df173b8e382c7e7c
Charlyln <charleshenry.lenue>
2020-07-23 11:10:07 +0200

    add media queries to appBar to put her in bottom page on smartphone

--- src/components/signUp/appBar/MyAppBar.jsx
+++ src/components/signUp/appBar/MyAppBar.jsx
@@ -7,34 +7,35 @@ import { Link, Redirect } from "react-router-dom";
 import PersonIcon from "@material-ui/icons/Person";
 import PostAddIcon from "@material-ui/icons/PostAdd";
 import FavoriteIcon from "@material-ui/icons/Favorite";
-import FlightTakeoffIcon from '@material-ui/icons/FlightTakeoff';
+import FlightTakeoffIcon from "@material-ui/icons/FlightTakeoff";
 
+import "./myAppBar.css";
 
 export default function MyAppBar() {
   return (
-    <AppBar position="fixed">
-      <Toolbar>
-        <Link to="/home">
+    <AppBar className="appBar">
+      <Toolbar style={{ width: "100%", padding: 0 }}>
+        <Link className="appLinks" to="/home">
           <IconButton color="white" aria-label="menu">
             <HomeIcon />
           </IconButton>
         </Link>
-        <Link to="/profil">
+        <Link className="appLinks" to="/profil">
           <IconButton color="white" aria-label="menu">
             <PersonIcon />
           </IconButton>
         </Link>
-        <Link to="/post">
+        <Link className="appLinks" to="/post">
           <IconButton color="white" aria-label="menu">
             <PostAddIcon />
           </IconButton>
         </Link>
-        <Link to="/favorite">
+        <Link className="appLinks" to="/favorite">
           <IconButton color="white" aria-label="menu">
             <FavoriteIcon />
           </IconButton>
         </Link>
-        <Link to="/bookings">
+        <Link className="appLinks" to="/bookings">
           <IconButton color="white" aria-label="menu">
             <FlightTakeoffIcon />
           </IconButton>
